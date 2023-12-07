18e1917324f327101e3fe6d84e6ff3c130f12e61
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-07 12:57:47 +0600

    Add Another File

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css'
-import SimpleForm from './Components/SimpleForm/SimpleForm'
+import "./App.css";
+import RefForm from "./Components/RefForm/RefForm";
+// import SimpleForm from "./Components/SimpleForm/SimpleForm";
 
 function App() {
-
   return (
     <>
-      
       <h2>React Form Submited Project</h2>
-      {/* <Forms></Forms> */}
-      <SimpleForm></SimpleForm>
+    {/* <SimpleForm></SimpleForm> */}
+    <RefForm></RefForm>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,17 +1,56 @@
+import { useState } from "react";
+
 const SimpleForm = () => {
-    const handleForm = e => {
-        e.preventDefault()
-    }
-    return (
-        <div>
-            <form onSubmit={handleForm}>
-                <input required type="text" name="" id="" placeholder="Type Your Name..." /><br />
-                <input required type="email" name="" id="" placeholder="E-mail Address.." /><br />
-                <input required type="password" name="" id="" placeholder="Password..." /><br />
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    );
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setpassword] = useState(null);
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log( 'name :', name, 'email :', email, 'password :', password);
+  };
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePass = (e) => {
+    setpassword(e.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={handleForm}>
+        <input
+          onChange={handleName}
+          required
+          type="text"
+          name="name"
+          id=""
+          placeholder="Type Your Name..."
+        />
+        <br />
+        <input
+          onChange={handleEmail}
+          required
+          type="email"
+          name=""
+          id=""
+          placeholder="E-mail Address.."
+        />
+        <br />
+        <input
+          onChange={handlePass}
+          required
+          type="password"
+          name=""
+          id=""
+          placeholder="Password..."
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default SimpleForm;
