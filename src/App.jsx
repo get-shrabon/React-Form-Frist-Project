2fff23f785b4fcf0056e05f90aece139a8bb4f0c
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-08 10:41:05 +0600

    Add Some File

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import RefForm from "./Components/RefForm/RefForm";
+import Shrestho from "./Components/SimpleForm/shrestho/shrestho";
 // import SimpleForm from "./Components/SimpleForm/SimpleForm";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <h2>React Form Submited Project</h2>
     {/* <SimpleForm></SimpleForm> */}
     <RefForm></RefForm>
+    <Shrestho></Shrestho>
     </>
   );
 }
