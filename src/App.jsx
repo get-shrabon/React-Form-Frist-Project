517fc34fcd7081589655973c726b63ab4afc70b7
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-09 11:52:50 +0600

    Delete Some File

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import RefForm from "./Components/RefForm/RefForm";
-import Shrestho from "./Components/SimpleForm/shrestho/shrestho";
 // import SimpleForm from "./Components/SimpleForm/SimpleForm";
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
       <h2>React Form Submited Project</h2>
     {/* <SimpleForm></SimpleForm> */}
     <RefForm></RefForm>
-    <Shrestho></Shrestho>
+   
     </>
   );
 }
